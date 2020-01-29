d2b96129d1e59611fddcab5c169953e87e1d76fb
Ronald Ordoñez <37679541+ronordonezq@users.noreply.github.com>
2020-01-29 03:36:05 -0500

    Login and Register Design B.

---
+++ app/src/main/java/com/example/easyserviceapp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.easyserviceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        //La actividad esta creada
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"OnStart",Toast.LENGTH_SHORT).show();
+        //La actividad esta apunto de hacerse visible
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+        //La actividad se ha vuelto visible
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
+        //Enfocarse en otra actividad, dejandonos en segundo plano
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+        //La actividad ya no es visible(esta "detenida")
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+        //La actividad esta apunto de ser destruida o cerrada
+    }
+}
